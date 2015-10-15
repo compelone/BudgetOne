f7cb1220f823a610a13a77bb5ca12a64b25c622e
compelone <lprivott@compelone.com>
2015-10-15 06:11:30 -0400

    a

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 // static content
-app.use(express.static(path.join(__dirname, '/dist')));
-app.use(express.static(path.join(__dirname, '/bower_components')));
+app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/bower_components'));
 
 // let pushState and the router do the navigation. return index.html for all non-static-content routes.
 app.get('/', function(req, res) {
