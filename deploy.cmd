9279a0404ccc1c669460538c738108f84692cfba
compelone <lprivott@compelone.com>
2015-10-15 07:13:50 -0400

    Added bower to dependencies and updated deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -111,7 +111,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
   echo "Installing Bower"
   call :ExecuteCmd !NPM_CMD! install bower
   if !ERRORLEVEL! NEQ 0 goto error
-  ./node_modules/.bin/bower install
+  call :ExecuteCmd "./node_modules/.bin/bower"
   if !ERRORLEVEL! NEQ 0 goto error
   popd
 )
