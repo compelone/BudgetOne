e73ce3245c532794a48ac957d01683da2194bd75
compelone <lprivott@compelone.com>
2015-10-17 10:26:55 -0400

    Added scripts static

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 // static content
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/scripts'));
 
 // let pushState and the router do the navigation. return index.html for all non-static-content routes.
 app.get('/', function(req, res) {
